dcaf54f5a9429b3e903b8455f67f5297c74db633
NDERI007 <tnderi373@gmail.com>
2025-05-19 08:48:12 +0300

    Done with Nav now onto pages

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,12 @@
+import chainsawMan from "../assets/1tkegtnpx5d31.jpg";
+
+function Homie() {
+  return (
+    <>
+      <div className="container w-screen">
+        <img src={chainsawMan} alt="chainsaw Man" />
+      </div>
+    </>
+  );
+}
+export default Homie;
