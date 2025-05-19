92837a930ebff43e68010b9756f5801ac8359f1b
NDERI007 <tnderi373@gmail.com>
2025-05-19 12:29:40 +0300

    best to use terminal when pushing multiple files

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,10 +1,19 @@
-import chainsawMan from "../assets/1tkegtnpx5d31.jpg";
+import chainsawMan from "../assets/Volume_11.jpg";
 
 function Homie() {
   return (
     <>
-      <div className="container w-screen">
-        <img src={chainsawMan} alt="chainsaw Man" />
+      <div className="container w-screen h-screen flex flex-col justify-items-center items-center">
+        <img src={chainsawMan} alt="chainsaw Man" className="w-xs md:w-lg" />
+        <div className="bg-[#1b1b32]">
+          <section className="p-3">
+            <p>
+              Hope you're having a good day, if you're curious about the cover
+              above the manga is called chainsaw Man but that's not our main
+              topic here,this a food website so order something will ya
+            </p>
+          </section>
+        </div>
       </div>
     </>
   );
---
+++ src/pages/menu.tsx
@@ -0,0 +1,9 @@
+import coverthir from "../assets/Volume_13.jpg";
+function Menu() {
+  return (
+    <>
+      <img src={coverthir} alt="" />
+    </>
+  );
+}
+export default Menu;
